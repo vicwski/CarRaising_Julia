8f30ed675af712266921c2bb832c34f7a7008f97
vicwski <90584286+vicwski@users.noreply.github.com>
2022-01-20 14:03:41 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,28 @@
+var canvas
+
+var gameState = 0
+var playerCount
+var allPlayers
+var distance = 0
+
+var database
+
+var form, player, game
+
+function setup() {
+  canvas = createCanvas(500, 500)
+  database = firebase.database()
+  game = new Game()
+  game.getState()
+  game.start()
+}
+
+function draw() {
+  if (playerCount === 4) {
+    game.update(1)
+  }
+  if (gameState === 1) {
+    clear()
+    game.play()
+  }
+}
